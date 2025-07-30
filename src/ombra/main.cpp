144123ba9a4096fd4c015dfe3737d711cff3afc5
JavierAR71 <javieramadorrodriguez98@gmail.com>
2025-07-30 20:21:38 +0200

    First commit
    Setup and first commit

---
+++ src/ombra/main.cpp
@@ -0,0 +1,25 @@
+﻿//
+// Created by Usuario on 30/07/2025.
+//
+
+#include "vulkan/vulkan.h"
+#include <iostream>
+
+namespace ombra {
+    void printVulkanVersion() {
+        uint32_t version = VK_API_VERSION_1_0;
+        VkResult result = vkEnumerateInstanceVersion(&version);
+
+        if (result == VK_SUCCESS) {
+            std::cout << "Vulkan version: " << version << std::endl;
+        } else {
+            std::cout << "Failed to retrieve Vulkan version" << std::endl;
+        }
+    }
+}
+
+int main() {
+    ombra::printVulkanVersion();
+    return 0;
+}
+//
